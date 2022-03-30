5b2e3066fc1599d8ae0856bcd5f4bf529546f262
Gareth Hind <garethjhind@gmail.com>
2022-03-30 12:17:31 +1100

    Added Favicon

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,13 @@
 export const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__info"></div>
-      <div className="footer__copyright">
+      <section className="footer__info">
+        <section className="footer__links"></section>
+        <section className="footer__icons"></section>
+      </section>
+      <section className="footer__copyright">
         Copyright &copy; Ant Software 2022
-      </div>
+      </section>
     </footer>
   );
 };
