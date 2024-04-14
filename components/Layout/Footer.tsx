e96e0907666b5020bf2f6154c5a5c2fa533377ba
Gareth Hind <garethjhind@gmail.com>
2024-04-14 18:27:00 +1000

    Refactor: Footer

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -14,39 +14,38 @@ const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <h2 className="heading-two | u-margin-bottom-small">
-          Sunderland Accounting
-        </h2>
+        <div>
+          <h2 className="heading-two | u-margin-bottom-small">
+            Sunderland Accounting
+          </h2>
+          {/* <section className="footer__icons | u-margin-bottom-medium">
+            <IconContext.Provider value={{}}>
+              <FaFacebook size={40} />
+              <FaGooglePlus size={40} />
+              <FaInstagram size={40} />
+              <FaLinkedin size={40} />
+            </IconContext.Provider>
+          </section> */}
+        </div>
 
-        <section className="footer__links | u-margin-bottom-medium ">
-          <ul role="list">
-            <li>Home</li>
-            <li>Our Services</li>
-            <li>About Us</li>
-          </ul>
-        </section>
-
-        <section className="footer__icons | u-margin-bottom-medium">
-          <IconContext.Provider value={{}}>
-            <FaFacebook size={40} />
-            <FaGooglePlus size={40} />
-            <FaInstagram size={40} />
-            <FaLinkedin size={40} />
-          </IconContext.Provider>
-        </section>
-
-        <section className="footer__links | u-margin-bottom-small">
-          <ul role="list">
-            <li>Contact Us</li>
-            <li>Terms</li>
-            <li>Privacy Policy</li>
-          </ul>
-        </section>
-
-        <section className="u-center-text">
-          Gareth Hind &copy; {currentYear}
-        </section>
+        <div>
+          <section className="footer__links | u-margin-bottom-small">
+            <ul role="list">
+              <li>Home</li>
+              <li>Our Services</li>
+              <li>About Us</li>
+            </ul>
+            <ul role="list">
+              <li>Contact Us</li>
+              <li>Terms</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </section>
+        </div>
       </div>
+      <section className="u-center-text">
+        Gareth Hind &copy; {currentYear}
+      </section>
     </footer>
   );
 };
